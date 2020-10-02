b3ba986aad99aab7b6d608e0fe7b77abe9b10f06
Utkarsh Tiwari <utiwari914@gmail.com>
2020-10-02 21:48:00 +0530

    make homepage responsive on mobile and reformat index.js

--- index.js
+++ index.js
@@ -1,102 +1,97 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const validUrl = require('valid-url')
-const ejs = require('ejs')
-const path = require('path')
-const shortid = require('shortid')
-const app = express()
-const { Url } = require('./models/url')
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const validUrl = require("valid-url");
+const ejs = require("ejs");
+const path = require("path");
+const shortid = require("shortid");
+const app = express();
+const { Url } = require("./models/url");
 
-app.use(express.json({extented:true}))
+app.use(express.json({ extented: true }));
 
 //bodyParser
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //===============db======================
-mongoose.connect("mongodb://localhost/url-shortener-api", {
+mongoose
+  .connect("mongodb://localhost/url-shortener-api", {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology:true
-    }).then(()=>{
-        console.log("MongoDB connected")
-    }).catch((err)=>{
-        console.log(err)
-    })
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-//========================================    
+//========================================
 
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname , 'public')))
-
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 3000;
 
 //BaseUrl
-const BaseUrl = 'https://url-shortener-app1.herokuapp.com/'
+const BaseUrl = "https://url-shortener-app1.herokuapp.com/";
 
 //=========Routes======================
 
-app.get('/',(req,res)=>{
-    res.render('index.ejs')
-})
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.post('/',async (req,res)=>{
-    
-    const longUrl = req.body.url
-    
-    const msgs = []
-    
-    const errors =[]
+app.post("/", async (req, res) => {
+  const longUrl = req.body.url;
+  const msgs = [];
+  const errors = [];
+  const urlCode = shortid.generate();
 
-    const urlCode = shortid.generate()
+  if (validUrl.isUri(longUrl)) {
+    const user = await Url.findOne({ longUrl });
 
-    if(validUrl.isUri(longUrl)){    
-        const user = await Url.findOne({longUrl})
+    if (user) {
+      //console.log(user)
+      msgs.push(user);
+      res.render("index.ejs", { msgs });
+    } else {
+      const shortUrl = BaseUrl + urlCode;
 
-        if(user){
-            //console.log(user)
-            msgs.push(user)
-            res.render('index.ejs',{msgs})
-        } else{
-        const shortUrl = BaseUrl + '/' + urlCode
+      const newUrl = new Url({
+        urlCode,
+        longUrl,
+        shortUrl,
+        date: new Date(),
+      });
 
-        const newUrl = new Url({
-           urlCode,
-           longUrl,
-           shortUrl,
-           date: new Date() 
-        })
-
-        await newUrl.save().then(()=>{
-            console.log("Url saved in db")
-            msgs.push(newUrl)
-            res.render('index.ejs',{msgs})
-        })
-
-        }
-    }else{
-        console.log("Enter valid Url")
-        errors.push({ error : "Enter an Valid Url. Try Again Later ."})
-        res.render('index.ejs',{errors})
-        console.log(errors.error)
+      await newUrl.save().then(() => {
+        console.log("Url saved in db");
+        msgs.push(newUrl);
+        res.render("index.ejs", { msgs });
+      });
     }
-})
+  } else {
+    console.log("Enter valid Url");
+    errors.push({ error: "Enter an Valid Url. Try Again Later ." });
+    res.render("index.ejs", { errors });
+    console.log(errors.error);
+  }
+});
 
 //get baseUrl/:code
-app.get('/:code', async (req,res)=>{
-    const userUrl = await Url.findOne({urlCode:req.params.code})
-    console.log(userUrl)
-    if(userUrl){
-        return res.redirect(userUrl.longUrl)
-    }else{
-        console.log("No url found")
-    }
-})
+app.get("/:code", async (req, res) => {
+  const userUrl = await Url.findOne({ urlCode: req.params.code });
+  console.log(userUrl);
+  if (userUrl) {
+    return res.redirect(userUrl.longUrl);
+  } else {
+    console.log("No url found");
+  }
+});
 
-
-app.listen(port ,()=>{
-    console.log('Server listening on port '+port)
-})
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
+});
